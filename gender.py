0a678829a220b5151b458525d34f8736b56ea578
satheesh2k14 <satheesh.prabhakaran97@gmail.com>
2017-03-14 17:38:58 +0530

    Gender classification with SVClassifier

--- gender.py
+++ gender.py
@@ -7,7 +7,7 @@ Created on Tue Mar 14 17:25:31 2017
 
 from sklearn import tree
 from sklearn.neighbors import  KNeighborsClassifier
-
+from sklearn.svm import SVC
 clf = tree.DecisionTreeClassifier()
 
 # [height, weight, shoe_size]
@@ -35,5 +35,16 @@ clf =  KNeighborsClassifier(3)
 clf = clf.fit(X, Y)
 
 prediction = clf.predict([[160,55,37]])
+
+print(prediction)
+
+#Classifier 3
+
+#Classifier 2
+clf =  SVC(kernel="linear", C=0.025)
+
+clf = clf.fit(X, Y)
+
+prediction = clf.predict([[175,75,37]])
 
 print(prediction)
